2ce45f9a51cba153375dcb4ec776a0e277a1de03
David Caro <david.caro.dec@hotmail.com>
2024-01-31 14:33:54 -0300

    Text button fix

--- src/app/update-project/page.tsx
+++ src/app/update-project/page.tsx
@@ -177,7 +177,7 @@ const Page = () => {
                         type="submit"
                         className="py-2 px-5 inline-block font-semibold tracking-wide border align-middle duration-500 text-base text-center bg-indigo-600 hover:bg-indigo-700 border-indigo-600 hover:border-indigo-700 text-white rounded-md"
                       >
-                        Crear proyecto
+                        Actualizar proyecto
                       </button>
                     </div>
                   </div>
